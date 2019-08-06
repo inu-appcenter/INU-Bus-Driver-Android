484606694ec5dd633784232a17b351ebec64a7f6
GowoonJ <jgw971229@naver.com>
2019-08-07 02:45:49 +0900

    add splash

--- app/src/main/java/inuappcenter/inubus_driver/Activity/Splash.kt
+++ app/src/main/java/inuappcenter/inubus_driver/Activity/Splash.kt
@@ -1,13 +1,29 @@
 package inuappcenter.inubus_driver.Activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import inuappcenter.inubus_driver.R
 
 class Splash : AppCompatActivity() {
 
+    val SPLASH_DISPLAY_LENGTH = 1300
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+                val intentSplash = Intent(this,Select::class.java)
+                this.startActivity(intentSplash)
+                this.finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
     }
 }
